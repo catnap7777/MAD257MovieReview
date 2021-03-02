c68efc29ec766075cb4698ee9c6a63fb2da3da59
km <35552602+catnap7777@users.noreply.github.com>
2021-03-01 21:34:48 -0600

    Initial Commit

---
+++ MAD257MovieReview/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MAD257MovieReview
+//
+//  Created by Karen Mathes on 3/1/21.
+//  Copyright © 2021 TygerMatrix Software. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
